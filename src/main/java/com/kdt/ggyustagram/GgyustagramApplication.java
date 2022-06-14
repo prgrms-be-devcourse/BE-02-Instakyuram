52502cb9dadf25f64199d7b0e570df07e94fe239
pjh <pjh_jn@naver.com>
2022-06-15 03:50:30 +0900

    setting : init project

---
+++ src/main/java/com/kdt/ggyustagram/GgyustagramApplication.java
@@ -0,0 +1,13 @@
+package com.kdt.ggyustagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgyustagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgyustagramApplication.class, args);
+	}
+
+}

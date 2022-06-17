2b1829f763664a4049ca21274d5755cf52d9faa2
yeon <soccer_by@naver.com>
2022-06-18 01:34:38 +0900

    [IK-94] fix:  MemberController import 에러 수정

--- src/main/java/com/kdt/instakyuram/member/controller/MemberController.java
+++ src/main/java/com/kdt/instakyuram/member/controller/MemberController.java
@@ -2,12 +2,10 @@ package com.kdt.instakyuram.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kdt.instakyuram.common.ApiResponse;
-import com.kdt.instakyuram.member.dto.MemberRequest;
 import com.kdt.instakyuram.member.dto.MemberResponse;
 import com.kdt.instakyuram.member.service.MemberService;
 

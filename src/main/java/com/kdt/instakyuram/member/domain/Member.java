07adc4605296d107d99129113890b94044f31cd5
pjh <pjh_jn@naver.com>
2022-06-17 15:17:14 +0900

    [IK-88] feat:  사용자 정보 조회 기능 추가

--- src/main/java/com/kdt/instakyuram/member/domain/Member.java
+++ src/main/java/com/kdt/instakyuram/member/domain/Member.java
@@ -1,18 +1,64 @@
 package com.kdt.instakyuram.member.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Member {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	private String name;
+	@Size(min= 6, max = 24)
+	private String username;
 
-	private String userName;
+	@Size(min= 6, max = 16)
+	private String name;
 
 	private String password;
 
-	private String phoneNunmber;
+	@Size(max = 11)
+	private String phoneNumber;
+
+	@Email
+	private String email;
+
+	protected Member() {
+	}
+
+	public Member(String username, String password, String name, String phoneNumber, String email) {
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
 }
---
+++ src/main/java/com/kdt/instakyuram/member/dto/MemberResponse.java
@@ -0,0 +1,4 @@
+package com.kdt.instakyuram.member.dto;
+
+public record MemberResponse(Long id, String username, String name, String email, String phoneNumber) {
+}

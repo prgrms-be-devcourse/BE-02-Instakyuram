3d77cb7eca84a0cb4f8a4c446a31d762496fd088
HyoungUkJJang <guddnr1451@gmail.com>
2022-06-18 20:06:16 +0900

    [IK-108] feat : PostImageService 구현

--- src/main/java/com/kdt/instakyuram/post/service/PostImageService.java
+++ src/main/java/com/kdt/instakyuram/post/service/PostImageService.java
@@ -1,7 +1,33 @@
 package com.kdt.instakyuram.post.service;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kdt.instakyuram.post.domain.PostImage;
+import com.kdt.instakyuram.post.domain.PostImageRepository;
+import com.kdt.instakyuram.post.dto.ImageUploader;
 
 @Service
+@Transactional(readOnly = true)
 public class PostImageService {
+
+	private final PostImageRepository postImageRepository;
+	private final ImageUploader imageUploader;
+
+	public PostImageService(PostImageRepository postImageRepository,
+		ImageUploader imageUploader) {
+		this.postImageRepository = postImageRepository;
+		this.imageUploader = imageUploader;
+	}
+
+	public List<PostImage> save(List<MultipartFile> images, Long postId) throws IOException {
+		List<PostImage> postImages = imageUploader.getPostImages(images, postId);
+
+		return postImageRepository.saveAll(postImages);
+	}
+
 }
--- src/main/java/com/kdt/instakyuram/post/service/PostService.java
+++ src/main/java/com/kdt/instakyuram/post/service/PostService.java
@@ -1,14 +1,17 @@
 package com.kdt.instakyuram.post.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kdt.instakyuram.member.domain.Member;
 import com.kdt.instakyuram.member.dto.MemberResponse;
 import com.kdt.instakyuram.member.service.MemberGiver;
 import com.kdt.instakyuram.post.domain.Post;
+import com.kdt.instakyuram.post.domain.PostImage;
 import com.kdt.instakyuram.post.domain.PostRepository;
 import com.kdt.instakyuram.post.dto.PostConverter;
 import com.kdt.instakyuram.post.dto.PostResponse;
@@ -20,15 +23,19 @@ public class PostService {
 	private final PostRepository postRepository;
 	private final PostConverter postConverter;
 	private final MemberGiver memberGiver;
+	private final PostImageService postImageService;
 
-	public PostService(PostRepository postRepository, PostConverter postConverter, MemberGiver memberGiver) {
+	public PostService(PostRepository postRepository, PostConverter postConverter,
+		MemberGiver memberGiver, PostImageService postImageService) {
 		this.postRepository = postRepository;
 		this.postConverter = postConverter;
 		this.memberGiver = memberGiver;
+		this.postImageService = postImageService;
 	}
 
 	@Transactional
-	public PostResponse.CreateResponse create(Long memberId, String content) {
+	public PostResponse.CreateResponse create(Long memberId, String content, List<MultipartFile> postImages) throws
+		IOException {
 		// 멤버 조회하기
 		Member member = postConverter.toMember(
 			memberGiver.findById(memberId)
@@ -36,8 +43,14 @@ public class PostService {
 
 		//userService.findById() 유저 정보 조회 해오기
 		Post savePost = postRepository.save(
-			new Post(content, member)
+			Post.builder()
+				.content(content)
+				.member(member)
+				.build()
 		);
+
+		// TODO 이미지 추가로직 처리해야함
+		List<PostImage> save = postImageService.save(postImages, savePost.getId());
 
 		return new PostResponse.CreateResponse(savePost.getId(), memberId, content);
 	}
@@ -45,7 +58,7 @@ public class PostService {
 	public List<PostResponse.FindAllResponse> findAll(Long memberId) {
 		List<MemberResponse> follows = memberGiver.findAllFollowing(memberId);
 		List<Member> members = follows.stream()
-			.map(follow -> postConverter.toMember(follow))
+			.map(postConverter::toMember)
 			.toList();
 
 		return postRepository.findByMemberIn(members).stream()

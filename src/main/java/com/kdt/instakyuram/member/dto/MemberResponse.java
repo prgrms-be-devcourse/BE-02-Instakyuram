3279f4f7b6b1c2c602ba973212701a2b5cdcfcce
pjh612 <pjhjh612@gmail.com>
2022-06-19 17:05:25 +0900

    [IK-23] feat:  로그인 기능 추가

--- src/main/java/com/kdt/instakyuram/member/dto/MemberResponse.java
+++ src/main/java/com/kdt/instakyuram/member/dto/MemberResponse.java
@@ -3,4 +3,7 @@ package com.kdt.instakyuram.member.dto;
 public record MemberResponse(Long id, String username, String name, String email, String phoneNumber) {
 	public record SignupResponse (Long id, String username) {
 	}
+
+	public record SigninResponse (Long id, String username, String accessToken, String refreshToken) {
+	}
 }
--- src/main/java/com/kdt/instakyuram/member/service/MemberService.java
+++ src/main/java/com/kdt/instakyuram/member/service/MemberService.java
@@ -8,6 +8,9 @@ import com.kdt.instakyuram.member.domain.Member;
 import com.kdt.instakyuram.member.domain.MemberRepository;
 import com.kdt.instakyuram.member.dto.MemberRequest;
 import com.kdt.instakyuram.member.dto.MemberResponse;
+import com.kdt.instakyuram.security.Role;
+import com.kdt.instakyuram.security.jwt.Jwt;
+import com.kdt.instakyuram.token.service.TokenService;
 
 // TODO : MemberGiver의 메서드가 필요합니다 !
 @Service
@@ -15,10 +18,15 @@ public class MemberService {
 
 	private final MemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
+	private final TokenService tokenService;
+	private final Jwt jwt;
 
-	public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+	public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder, TokenService tokenService,
+		Jwt jwt) {
 		this.memberRepository = memberRepository;
 		this.passwordEncoder = passwordEncoder;
+		this.tokenService = tokenService;
+		this.jwt = jwt;
 	}
 
 	public MemberResponse findById(Long id) {
@@ -43,5 +51,20 @@ public class MemberService {
 
 		return new MemberResponse.SignupResponse(member.getId(), member.getUsername());
 	}
+
+	public MemberResponse.SigninResponse signin(String username, String password) {
+		Member foundMember = memberRepository.findByUsername(username)
+			.orElseThrow(() -> new NotFoundException("유저 정보가 일치하지 않습니다."));
+		if (!passwordEncoder.matches(password, foundMember.getPassword())) {
+			throw new NotFoundException("유저 정보가 일치하지 않습니다.");
+		}
+		String[] roles = {String.valueOf(Role.MEMBER)};
+		String accessToken = jwt.generateAccessToken(Jwt.Claims.from(foundMember.getId().toString(), roles));
+		String refreshToken = jwt.generateRefreshToken();
+		tokenService.save(refreshToken, foundMember.getId());
+
+		return new MemberResponse.SigninResponse(foundMember.getId(), username, accessToken, refreshToken);
+	}
 }
+
 

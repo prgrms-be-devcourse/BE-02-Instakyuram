b1a6e92b753d350391b153bf398c0cf8d4d59017
pjh612 <pjhjh612@gmail.com>
2022-06-21 01:06:54 +0900

    [IK-23] test:  MemberRepositoryTest 추가

---
+++ src/test/java/com/kdt/instakyuram/configures/WebSecurityTestConfigure.java
@@ -0,0 +1,13 @@
+package com.kdt.instakyuram.configures;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+
+import com.kdt.instakyuram.security.WebSecurityConfigure;
+import com.kdt.instakyuram.security.jwt.JwtConfigure;
+import com.kdt.instakyuram.token.service.TokenService;
+
+@Import({WebSecurityConfigure.class, TokenService.class})
+@EnableConfigurationProperties(JwtConfigure.class)
+public class WebSecurityTestConfigure {
+}
---
+++ src/test/java/com/kdt/instakyuram/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,216 @@
+package com.kdt.instakyuram.member.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.kdt.instakyuram.configures.WebSecurityTestConfigure;
+import com.kdt.instakyuram.member.domain.Member;
+import com.kdt.instakyuram.member.domain.MemberRepository;
+
+@DataJpaTest
+@Import(WebSecurityTestConfigure.class)
+class MemberRepositoryTest {
+
+	@Autowired
+	TestEntityManager entityManager;
+
+	@Autowired
+	MemberRepository memberRepository;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Test
+	@DisplayName("Member 추가 성공 테스트")
+	void testSaveSuccess() {
+		//given
+		String encodedPassword = passwordEncoder.encode("@Password12");
+		Member member = new Member(
+			"pjh123",
+			encodedPassword,
+			"홍길동",
+			"01012345678",
+			"user@gmail.com");
+
+		//when
+		Member savedMember = memberRepository.save(member);
+
+		//then
+		assertThat(savedMember.getId()).isNotNull();
+		assertThat(savedMember.getUsername()).isEqualTo(member.getUsername());
+		assertThat(savedMember.getPassword()).isEqualTo(member.getPassword());
+		assertThat(savedMember.getName()).isEqualTo(member.getName());
+		assertThat(savedMember.getEmail()).isEqualTo(member.getEmail());
+		assertThat(savedMember.getPhoneNumber()).isEqualTo(member.getPhoneNumber());
+
+	}
+
+	@Test
+	@DisplayName("중복된 아이디로 가입시 실패 테스트")
+	void testSaveWithDuplicatedUsername() {
+		//given
+		String encodedPassword = passwordEncoder.encode("@Password12");
+		Member memberA = new Member(
+			"user123",
+			encodedPassword,
+			"홍길동",
+			"01012345678",
+			"userA@gmail.com");
+		Member memberB = new Member(
+			memberA.getUsername(),
+			encodedPassword,
+			"김길동",
+			"01012345678",
+			"userB@gmail.com");
+		memberRepository.save(memberA);
+
+		//when, then
+		assertThatThrownBy(() -> memberRepository.save(memberB)).isInstanceOf(DataIntegrityViolationException.class);
+	}
+
+	@Test
+	@DisplayName("중복된 이메일로 가입시 실패 테스트")
+	void testSaveWithDuplicatedEmail() {
+		//given
+		String encodedPassword = passwordEncoder.encode("@Password12");
+		Member memberA = new Member(
+			"user123",
+			encodedPassword,
+			"홍길동",
+			"01012345678",
+			"user@gmail.com");
+		Member memberB = new Member(
+			"user124",
+			encodedPassword,
+			"김길동",
+			"01012345678",
+			memberA.getEmail());
+		memberRepository.save(memberA);
+
+		//when, then
+		assertThatThrownBy(() -> memberRepository.save(memberB)).isInstanceOf(DataIntegrityViolationException.class);
+	}
+
+	@Test
+	@DisplayName("이메일정보 null로 가입시 실패 테스트")
+	void testSaveWithNullEmail() {
+		//given
+		String encodedPassword = passwordEncoder.encode("@Password12");
+		Member member = new Member(
+			"user123",
+			encodedPassword,
+			"홍길동",
+			"01012345678",
+			null);
+
+		//when, then
+		assertThatThrownBy(() -> memberRepository.save(member)).isInstanceOf(ConstraintViolationException.class);
+	}
+
+	@Test
+	@DisplayName("Username null로 가입시 실패 테스트")
+	void testSaveWithNullUsername() {
+		//given
+		String encodedPassword = passwordEncoder.encode("@Password12");
+		Member member = new Member(
+			null,
+			encodedPassword,
+			"홍길동",
+			"01012345678",
+			"user123@gmail.com");
+
+		//when, then
+		assertThatThrownBy(() -> memberRepository.save(member)).isInstanceOf(ConstraintViolationException.class);
+	}
+
+	@Test
+	@DisplayName("Username blank로 가입시 실패 테스트")
+	void testSaveWithBlankUsername() {
+		//given
+		String encodedPassword = passwordEncoder.encode("@Password12");
+		Member member = new Member(
+			"",
+			encodedPassword,
+			"홍길동",
+			"01012345678",
+			"user123@gmail.com");
+
+		//when, then
+		assertThatThrownBy(() -> memberRepository.save(member)).isInstanceOf(ConstraintViolationException.class);
+	}
+
+	@Test
+	@DisplayName("대문자 포함 Username 가입시 실패 테스트")
+	void testSaveWithIncludeCapitalUsername() {
+		//given
+		String encodedPassword = passwordEncoder.encode("@Password12");
+		Member member = new Member(
+			"Pjh123",
+			encodedPassword,
+			"홍길동",
+			"01012345678",
+			"user123@gmail.com");
+
+		//when, then
+		assertThatThrownBy(() -> memberRepository.save(member)).isInstanceOf(ConstraintViolationException.class);
+	}
+
+	@Test
+	@DisplayName("name null로 가입시 실패 테스트")
+	void testSaveWithNullName() {
+		//given
+		String encodedPassword = passwordEncoder.encode("@Password12");
+		Member member = new Member(
+			"user123",
+			encodedPassword,
+			null,
+			"01012345678",
+			"user123@gmail.com");
+
+		//when, then
+		assertThatThrownBy(() -> memberRepository.save(member)).isInstanceOf(ConstraintViolationException.class);
+	}
+
+	@Test
+	@DisplayName("name blank로 가입시 실패 테스트")
+	void testSaveWithBlankName() {
+		//given
+		String encodedPassword = passwordEncoder.encode("@Password12");
+		Member member = new Member(
+			"user123",
+			encodedPassword,
+			"",
+			"01012345678",
+			"user123@gmail.com");
+
+		//when, then
+		assertThatThrownBy(() -> memberRepository.save(member)).isInstanceOf(ConstraintViolationException.class);
+	}
+
+	@Test
+	@DisplayName("phoneNumber null로 가입시 실패 테스트")
+	void testSaveWithNullPhoneNumber() {
+		//given
+		String encodedPassword = passwordEncoder.encode("@Password12");
+		Member member = new Member(
+			"user123",
+			encodedPassword,
+			"홍길동",
+			null,
+			"user123@gmail.com");
+
+		//when, then
+		assertThatThrownBy(() -> memberRepository.save(member)).isInstanceOf(ConstraintViolationException.class);
+	}
+}

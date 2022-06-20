646b37114dc96d08a500ffe99fce56a67d9da089
pjh612 <pjhjh612@gmail.com>
2022-06-21 04:01:16 +0900

    [IK-23] feat:  findByUsername 인터페이스에 추가

--- src/main/java/com/kdt/instakyuram/member/domain/MemberRepository.java
+++ src/main/java/com/kdt/instakyuram/member/domain/MemberRepository.java
@@ -1,10 +1,13 @@
 package com.kdt.instakyuram.member.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	List<Member> findByIdIn(List<Long> memberIds);
+
+	Optional<Member> findByUsername(String username);
 }

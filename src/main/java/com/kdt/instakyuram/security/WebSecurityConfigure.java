4d8e349625dd60cb9515a19e1910cb332b30d3a8
pjh612 <pjhjh612@gmail.com>
2022-06-21 01:07:39 +0900

    [IK-23] setting:  JWT관련 프로퍼티 설정 클래스를 WebSecurityConfigure에 병합

--- src/main/java/com/kdt/instakyuram/security/WebSecurityConfigure.java
+++ src/main/java/com/kdt/instakyuram/security/WebSecurityConfigure.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,6 +24,7 @@ import com.kdt.instakyuram.token.service.TokenService;
 
 @EnableWebSecurity
 @Configuration
+@ConfigurationPropertiesScan("com.kdt.instakyuram.security")
 public class WebSecurityConfigure {
 	private final Logger log = LoggerFactory.getLogger(getClass());
 

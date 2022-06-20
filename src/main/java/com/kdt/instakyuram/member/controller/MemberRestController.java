3f5739f948152ed1d4472d43efdd329eac882cd9
pjh612 <pjhjh612@gmail.com>
2022-06-21 04:01:58 +0900

    [IK-23] feat:  sign in 기능 추가

--- src/main/java/com/kdt/instakyuram/member/controller/MemberRestController.java
+++ src/main/java/com/kdt/instakyuram/member/controller/MemberRestController.java
@@ -1,5 +1,8 @@
 package com.kdt.instakyuram.member.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.ResponseCookie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,15 +14,18 @@ import com.kdt.instakyuram.common.ApiResponse;
 import com.kdt.instakyuram.member.dto.MemberRequest;
 import com.kdt.instakyuram.member.dto.MemberResponse;
 import com.kdt.instakyuram.member.service.MemberService;
+import com.kdt.instakyuram.security.jwt.JwtConfigure;
 
 @RestController
 @RequestMapping("/api/members")
 public class MemberRestController {
 
 	private final MemberService memberService;
+	private final JwtConfigure jwtConfigure;
 
-	public MemberRestController(MemberService memberService) {
+	public MemberRestController(MemberService memberService, JwtConfigure jwtConfigure) {
 		this.memberService = memberService;
+		this.jwtConfigure = jwtConfigure;
 	}
 
 	@GetMapping("/{userId}")
@@ -31,5 +37,21 @@ public class MemberRestController {
 	ApiResponse<MemberResponse.SignupResponse> signup(@RequestBody MemberRequest.SignupRequest request) {
 		return new ApiResponse<>(memberService.signup(request));
 	}
+
+	@PostMapping("/signin")
+	public ApiResponse<MemberResponse.SigninResponse> signIn(@RequestBody MemberRequest.SignupRequest request,
+		HttpServletResponse response) {
+		MemberResponse.SigninResponse signinResponse = this.memberService.signin(request.username(),
+			request.password());
+		ResponseCookie accessTokenCookie = ResponseCookie.from(jwtConfigure.accessToken().header(),
+			signinResponse.accessToken()).build();
+		ResponseCookie refreshTokenCookie = ResponseCookie.from(jwtConfigure.refreshToken().header(),
+			signinResponse.refreshToken()).build();
+		response.setHeader("Set-Cookie", accessTokenCookie.toString());
+		response.addHeader("Set-Cookie", refreshTokenCookie.toString());
+
+		return new ApiResponse<>(signinResponse);
+	}
+
 
 }
--- src/main/java/com/kdt/instakyuram/member/dto/MemberRequest.java
+++ src/main/java/com/kdt/instakyuram/member/dto/MemberRequest.java
@@ -3,4 +3,7 @@ package com.kdt.instakyuram.member.dto;
 public record MemberRequest() {
 	public record SignupRequest(String username, String password, String name, String email, String phoneNumber) {
 	}
+
+	public record SigninRequest(String username, String password) {
+	}
 }

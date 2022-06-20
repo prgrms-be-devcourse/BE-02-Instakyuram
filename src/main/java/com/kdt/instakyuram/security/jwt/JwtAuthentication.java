4429b292c26a503db8e11bd6d4a014cde5e37609
pjh612 <pjhjh612@gmail.com>
2022-06-21 04:17:01 +0900

    [IK-23] refactor:  principal 객체 username -> id

--- src/main/java/com/kdt/instakyuram/security/jwt/JwtAuthentication.java
+++ src/main/java/com/kdt/instakyuram/security/jwt/JwtAuthentication.java
@@ -3,10 +3,10 @@ package com.kdt.instakyuram.security.jwt;
 import static com.google.common.base.Preconditions.checkArgument;
 import static org.apache.logging.log4j.util.Strings.isNotEmpty;
 
-public record JwtAuthentication(String token, String username) {
+public record JwtAuthentication(String token, String id) {
 	public JwtAuthentication {
 		checkArgument(isNotEmpty(token), "token must be provided");
-		checkArgument(isNotEmpty(token), "username must be provided");
+		checkArgument(isNotEmpty(id), "id must be provided");
 	}
 
 }

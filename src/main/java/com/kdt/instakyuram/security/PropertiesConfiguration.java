68fbc77d15f80600e1021640ea4d851d8ca29b90
pjh612 <pjhjh612@gmail.com>
2022-06-21 15:13:37 +0900

    [IK-154] refactor:  불필요 설정파일 삭제

--- src/main/java/com/kdt/instakyuram/security/PropertiesConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.kdt.instakyuram.security;
-
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationPropertiesScan("com.kdt.instakyuram.security")
-public class PropertiesConfiguration {
-}

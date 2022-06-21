fa73395622b66983f0bbe56eee05eab19d68994a
pjh612 <pjhjh612@gmail.com>
2022-06-21 15:13:20 +0900

    [IK-154] setting:  정적 리소스 ignoring 추가

--- src/main/java/com/kdt/instakyuram/security/WebSecurityConfigure.java
+++ src/main/java/com/kdt/instakyuram/security/WebSecurityConfigure.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -80,6 +81,11 @@ public class WebSecurityConfigure {
 			response.getWriter().flush();
 			response.getWriter().close();
 		};
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return web -> web.ignoring().antMatchers("/static/**", "/templates/**");
 	}
 
 	@Bean

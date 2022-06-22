1e6c6578c7221f03251f836f9468fb8c0926382b
pjh612 <pjhjh612@gmail.com>
2022-06-23 04:44:13 +0900

    [IK-176] feat:  follow, unfollow, 내가 팔로우하고 있는지 확인하는 api 추가

---
+++ src/main/java/com/kdt/instakyuram/follow/controller/FollowRestController.java
@@ -0,0 +1,39 @@
+package com.kdt.instakyuram.follow.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kdt.instakyuram.common.ApiResponse;
+import com.kdt.instakyuram.follow.service.FollowService;
+import com.kdt.instakyuram.security.jwt.JwtAuthentication;
+
+@RestController
+@RequestMapping("/api/friendships")
+public class FollowRestController {
+
+	private final FollowService followService;
+
+	public FollowRestController(FollowService followService) {
+		this.followService = followService;
+	}
+
+	@GetMapping("/{memberId}")
+	public ApiResponse<Boolean> isFollowed(@AuthenticationPrincipal JwtAuthentication auth,
+		@PathVariable Long memberId) {
+		return new ApiResponse<>(followService.isFollowed(Long.parseLong(auth.id()), memberId));
+	}
+
+	@GetMapping("/follow/{memberId}")
+	public ApiResponse<Boolean> follow(@AuthenticationPrincipal JwtAuthentication auth, @PathVariable Long memberId) {
+		return new ApiResponse<>(followService.follow(Long.valueOf(auth.id()), memberId));
+	}
+
+	@GetMapping("/unfollow/{memberId}")
+	public ApiResponse<Boolean> unfollow(@AuthenticationPrincipal JwtAuthentication auth, @PathVariable Long memberId) {
+		followService.unFollow(Long.valueOf(auth.id()), memberId);
+		return new ApiResponse<>(true);
+	}
+}

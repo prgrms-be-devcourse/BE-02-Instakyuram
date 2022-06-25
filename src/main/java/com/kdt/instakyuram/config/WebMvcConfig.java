fe88c00f270e006d388332c8e997e05cfaf2c450
midasworld <midas117@knou.ac.kr>
2022-06-25 11:40:04 +0900

    [IK-250] refactor: WebMvcConfig - 설정빈 스캔 어노테이션 변경 - `@ConfigurationPropertiesScan` → `@EnableConfigurationProperties`

--- src/main/java/com/kdt/instakyuram/config/WebMvcConfig.java
+++ src/main/java/com/kdt/instakyuram/config/WebMvcConfig.java
@@ -1,12 +1,12 @@
 package com.kdt.instakyuram.config;
 
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ConfigurationPropertiesScan("com.kdt.instakyuram.config")
+@EnableConfigurationProperties({CorsConfigure.class})
 public class WebMvcConfig implements WebMvcConfigurer {
 
 	private final CorsConfigure corsConfigure;

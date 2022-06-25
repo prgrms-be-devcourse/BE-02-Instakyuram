71a5853c35053e1892f16e4ea2cabff83c87d75f
midasworld <midas117@knou.ac.kr>
2022-06-25 21:10:30 +0900

    [IK-257] refactor: flyway - 시드 파일 INSERT 문에 IGNORE 속성 적용

--- src/main/resources/db/seed/R__011_Seed_post_like.sql
+++ src/main/resources/db/seed/R__011_Seed_post_like.sql
@@ -1,7 +1,7 @@
-INSERT INTO post_like (post_id, member_id) VALUES (1, 2);
-INSERT INTO post_like (post_id, member_id) VALUES (1, 3);
-INSERT INTO post_like (post_id, member_id) VALUES (1, 4);
-INSERT INTO post_like (post_id, member_id) VALUES (1, 5);
-INSERT INTO post_like (post_id, member_id) VALUES (3, 1);
-INSERT INTO post_like (post_id, member_id) VALUES (3, 4);
-INSERT INTO post_like (post_id, member_id) VALUES (7, 1);
+INSERT IGNORE INTO post_like (post_id, member_id) VALUES (1, 2);
+INSERT IGNORE INTO post_like (post_id, member_id) VALUES (1, 3);
+INSERT IGNORE INTO post_like (post_id, member_id) VALUES (1, 4);
+INSERT IGNORE INTO post_like (post_id, member_id) VALUES (1, 5);
+INSERT IGNORE INTO post_like (post_id, member_id) VALUES (3, 1);
+INSERT IGNORE INTO post_like (post_id, member_id) VALUES (3, 4);
+INSERT IGNORE INTO post_like (post_id, member_id) VALUES (7, 1);

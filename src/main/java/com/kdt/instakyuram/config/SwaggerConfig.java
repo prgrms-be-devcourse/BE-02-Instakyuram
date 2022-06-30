d9cabd6e23f1e6852b17861e44d568507da84fc0
킹받드라슈 <soccer_by@naver.com>
2022-06-30 21:50:31 +0900

    [IK-267] feat:  swagger 설정에 필요한 상수 yml 바인딩 변경

--- src/main/java/com/kdt/instakyuram/config/SwaggerConfig.java
+++ src/main/java/com/kdt/instakyuram/config/SwaggerConfig.java
@@ -1,9 +1,6 @@
 package com.kdt.instakyuram.config;
 
-import static java.util.Collections.singletonList;
-
-import java.util.List;
-
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,20 +8,25 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
+@EnableConfigurationProperties({SwaggerConfigProperties.class})
 public class SwaggerConfig {
 
+	private final SwaggerConfigProperties swaggerConfigProperties;
+
+	public SwaggerConfig(SwaggerConfigProperties swaggerConfigProperties) {
+		this.swaggerConfigProperties = swaggerConfigProperties;
+	}
+
 	private ApiInfo swaggerInfo() {
-		return new ApiInfoBuilder().title("KkyuTeam-Instagram")
-			.version("v1.0.0")
-			.description("api docs swagger ")
+		return new ApiInfoBuilder().title(swaggerConfigProperties.title())
+			.version(swaggerConfigProperties.version())
+			.description(swaggerConfigProperties.description())
 			.build();
 	}
 
@@ -32,28 +34,10 @@ public class SwaggerConfig {
 	public Docket swaggerApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
 			.apiInfo(swaggerInfo()).select()
-			.apis(RequestHandlerSelectors.basePackage("com.kdt.instakyuram"))
-			.paths(PathSelectors.any())
+			.apis(RequestHandlerSelectors.basePackage(swaggerConfigProperties.basePackage()))
+			.paths(PathSelectors.ant(swaggerConfigProperties.apiPath()))
 			.build()
-			.securitySchemes(singletonList(apiKeyAccessToken()))
 			.useDefaultResponseMessages(false);
-
-	}
-
-	// note : swagger - ui를 에러 발생.
-	private List<SecurityScheme> getTokensApiKey() {
-		return List.of(apiKeyAccessToken(), apiKeyRefreshToken());
-	}
-
-	/**
-	 * todo : 인증 쿠키 셋팅하기 [현재 2개 accessToken, refreshToken 설정하려는데 1개만 들어가고 있음.] from: whyWhale
-	 * @return
-	 */
-	private ApiKey apiKeyAccessToken() {
-		return (new ApiKey("Authorization", "aToken", "cookie"));
-	}
-
-	private ApiKey apiKeyRefreshToken() {
-		return (new ApiKey("Authorization", "rToken", "cookie"));
 	}
 }
+

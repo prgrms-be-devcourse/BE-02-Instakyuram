61acfd24250688c24286b820498f7787dc78c3db
킹받드라슈 <soccer_by@naver.com>
2022-07-01 22:11:08 +0900

    test: test

---
+++ src/test/java/com/kdt/instakyuram/Workflow.java
@@ -0,0 +1,17 @@
+package com.kdt.instakyuram;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Workflow {
+	@Test
+	@DisplayName("테스트")
+	void testCall() {
+		//given
+
+		//when
+		Assertions.assertEquals(1, 10);
+		//then
+	}
+}

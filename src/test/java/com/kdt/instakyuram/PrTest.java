7c0f4e1e76fef6a34e27d7e90f440dbe435b847d
킹받드라슈 <soccer_by@naver.com>
2022-07-01 19:56:43 +0900

    [IK-315] test:  prTest 재작성

--- src/test/java/com/kdt/instakyuram/PrTest.java
+++ src/test/java/com/kdt/instakyuram/PrTest.java
@@ -8,12 +8,26 @@ public class PrTest {
 
 	@Test
 	@DisplayName("일부러 틀려버리기!")
-	void failTest(){
-	    //given
+	void failTest() {
+		//given
+		int value = 100;
+		testing expectedTest = new testing(value);
+		//when
 
-	    //when
+		//then
+		Assertions.assertThat(expectedTest.getValue()).isEqualTo(value);
+	}
 
-	    //then
-		Assertions.assertThat(1).isEqualTo(10);
+}
+
+class testing {
+	private final int value;
+
+	testing(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value * 3;
 	}
 }

b0b4455b22eac4b329d22cd695a0d244dd3432d3
HYEBPARK <hyeqls3742@gmail.com>
2022-07-02 13:33:18 +0900

    [IK-307] refactor : swagger open API 로 수정

--- src/main/java/com/kdt/instakyuram/user/member/controller/ProfileRestController.java
+++ src/main/java/com/kdt/instakyuram/user/member/controller/ProfileRestController.java
@@ -10,15 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.kdt.instakyuram.common.ApiResponse;
-import com.kdt.instakyuram.user.member.service.ProfileService;
 import com.kdt.instakyuram.security.jwt.JwtAuthentication;
+import com.kdt.instakyuram.user.member.service.ProfileService;
 
-import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Api(tags= "사용자 이미지 api")
+@Tag(name = "사용자 이미지 api")
 @RestController
 @RequestMapping("/api/profiles")
 public class ProfileRestController {
@@ -29,20 +27,10 @@ public class ProfileRestController {
 		this.profileService = profileService;
 	}
 
-
-	@Operation(
-		summary = "사용자 이미지 로딩",
-		description = "자신 또는 상대방의 이미지 파일을 웹 브라우저에 전달합니다.",
-		responses = {
-			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "이미지 로딩 성공."),
-			@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "500", description = "이미지 로딩 중 문제 발생."),
-		}
-	)
+	@Operation(summary = "사용자 이미지 로딩", description = "자신 또는 상대방의 이미지 파일을 웹 브라우저에 전달합니다.")
 	@GetMapping("/{memberId}/image")
 	public FileSystemResource get(
-		@Parameter(
-			name = "이미지를 소유한 사용자 식별 값", description = "사용자 id 값(숫자)을 입력합니다.", in = ParameterIn.PATH, required = true
-		)@PathVariable("memberId") Long id) {
+		@PathVariable("memberId") Long id) {
 		return this.profileService.findProfileImage(id);
 	}
 

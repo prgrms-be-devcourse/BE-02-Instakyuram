9c95e7496f7106173f1804752623e1119db3e555
킹받드라슈 <soccer_by@naver.com>
2022-07-02 18:25:44 +0900

    [IK-315] test:  work flow test

---
+++ src/test/java/com/kdt/instakyuram/PrTest.java
@@ -0,0 +1,32 @@
+package com.kdt.instakyuram;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PrTest {
+	@Test
+	@DisplayName("테스트 실패 하기")
+	void failTest() {
+		//given
+		int input = 100;
+		pr test = new pr(input);
+		//when
+		int gettingVal = test.getVal();
+		//then
+
+		Assertions.assertThat(gettingVal).isEqualTo(input);
+	}
+}
+
+class pr {
+	private final int val;
+
+	public pr(int val) {
+		this.val = val * 10;
+	}
+
+	public int getVal() {
+		return val;
+	}
+}

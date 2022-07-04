c9e849b442341bec9d6afbece2e7489abb59fa91
HyoungUkJJang <guddnr1451@gmail.com>
2022-07-04 23:33:32 +0900

    [IK-328] feat : 게시글 수정 UI 작성 및 API 연동하기

--- src/main/java/com/kdt/instakyuram/article/post/controller/PostController.java
+++ src/main/java/com/kdt/instakyuram/article/post/controller/PostController.java
@@ -4,11 +4,20 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.kdt.instakyuram.article.post.dto.PostResponse;
+import com.kdt.instakyuram.article.post.service.PostService;
 import com.kdt.instakyuram.security.jwt.JwtAuthentication;
 
 @Controller
 public class PostController {
+
+	private final PostService postService;
+
+	public PostController(PostService postService) {
+		this.postService = postService;
+	}
 
 	@GetMapping("/posts")
 	public String postsPage() {
@@ -35,4 +44,17 @@ public class PostController {
 
 		return "index";
 	}
+
+	@GetMapping("/posts/{id}/update")
+	public String postUpdate(
+		@PathVariable Long id,
+		@AuthenticationPrincipal JwtAuthentication auth, Model model
+	) {
+		PostResponse.FindAllResponse post = postService.findById(auth.id(), id);
+		model.addAttribute("member", auth);
+		model.addAttribute("post", post);
+
+		return "/modal/post-update";
+	}
+
 }
